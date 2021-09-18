6d60cdf25109b9ddd51ba20fe02f9befbeeea543
Asif Azad <asifazad799@gmail.com>
2021-09-18 12:19:47 +0530

    twitter

---
+++ script.js
@@ -0,0 +1,7 @@
+textarea = document.querySelector("#typing_sec");
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
